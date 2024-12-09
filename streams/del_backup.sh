ae053919c8368905b13f3ba0133470f37f8de92b
Tchangalo <153798615+Tchangalo@users.noreply.github.com>
2024-12-09 03:49:59 +0100

    Add files via upload

---
+++ streams/del_backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+provider=$1
+first_router=$2
+last_router=$3
+
+C='\033[0;94m'
+G='\033[0;32m'
+L='\033[38;5;135m'
+NC='\033[0m'
+
+if [ -z "$provider" ] || [ -z "$first_router" ] || [ -z "$last_router" ]; then
+    echo -e "${R}Error: At least one variable empty!${NC}"
+    exit 1
+fi
+
+echo -e "${C}Deleting ALL existing backups${NC}"
+if [[ $(df -T / | awk 'NR==2 {print $2}') == "zfs" ]]; then
+	sudo rm -rf /var/lib/vz/dump
+	sudo mkdir /var/lib/vz/dump
+	for i in $(seq $first_router $last_router); do
+		vmid=${provider}0${provider}0$(printf '%02d' $i)
+		echo -e "${C}Backing up router ${vmid}${NC}"
+		sudo vzdump ${provider}0${provider}00$i --dumpdir /var/lib/vz/dump --mode snapshot --compress 0
+		sleep 5
+	done
+else
+	sudo rm -rf /var/lib/pve/local-btrfs/dump
+	sudo mkdir /var/lib/pve/local-btrfs/dump
+	for i in $(seq $first_router $last_router); do
+		vmid=${provider}0${provider}0$(printf '%02d' $i)
+		echo -e "${C}Backing up router ${vmid}${NC}"
+		sudo vzdump ${provider}0${provider}00$i --dumpdir /var/lib/pve/local-btrfs/dump --mode snapshot --compress 0
+		sleep 5
+	done
+fi
+
+if [[ $first_router == $last_router ]]; then
+	echo -e "${G}Deletion of ALL existing backups and backup of router ${L}p${provider}r${first_router}v${G} executed successfully!${NC}"
+else
+	echo -e "${G}Deletion of ALL existing backups and backups of routers ${L}p${provider}r${first_router}v${G} to ${L}p${provider}r${last_router}v${G} executed successfully!${NC}"
+fi
